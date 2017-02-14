b404640dc58e3f2e12758eb7002a1524e019a5cf
gurshane <gurshane.sidhu@mail.mcgill.ca>
2017-02-14 13:34:52 -0500

    Board generates according to game rules.

--- Assets/Scripts/BoardGenerator.cs
+++ Assets/Scripts/BoardGenerator.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class BoardGenerator : MonoBehaviour {
 
+    //Spawn Locations
+    public List<Transform> mainBoardHexSpawns;
+    public List<Transform> islandHexSpawns;
+
     //Hexes
-    public List<Transform> hexSpawnLocations;
     public List<GameObject> hexPieces;
+
+    //Hex Prefabs
+    public GameObject goldHex;
+    public GameObject forestHex;
+    public GameObject pastureHex;
+    public GameObject mountainHex;
+    public GameObject hillHex;
+    public GameObject dessertHex;
+    public GameObject fieldHex;
 
     //Number Tiles
     public List<Transform> numberSpawnLocations;
@@ -21,49 +33,73 @@ public class BoardGenerator : MonoBehaviour {
     //Spawned versions of the hexes, number tiles, water hexes
     private List<GameObject> spawned;
     private List<GameObject> spawnedNumbers;
-    private List<GameObject> spawnedWaterTiles;
+    private List<GameObject> spawnedMainBoardHexes;
+    private List<GameObject> spawnedIslandHexes;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
         spawned = new List<GameObject>();
         spawnedNumbers = new List<GameObject>();
+        spawnedMainBoardHexes = new List<GameObject>();
+        spawnedIslandHexes = new List<GameObject>();
 
         makeBoard();
 	}
 
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            makeBoard();
-        }
-    }
-	
 	void makeBoard()
     {
-        if(spawned.Count > 0)
+        //Spawn Main Board Hexes
+        //----------------------
+        //Spawn Forest
+        spawnHex(mainBoardHexSpawns, forestHex, 4, true);
+        //Spawn Fields
+        spawnHex(mainBoardHexSpawns, fieldHex, 4, true);
+        //Spawn Hills
+        spawnHex(mainBoardHexSpawns, hillHex, 3, true);
+        //Spawn Dessert
+        spawnHex(mainBoardHexSpawns, dessertHex, 1, true);
+        //Spawn Mountain 
+        spawnHex(mainBoardHexSpawns, mountainHex, 3, true);
+        //Spawn Pasture 
+        spawnHex(mainBoardHexSpawns, pastureHex, 4, true);
+
+
+        //Spawn Island Hexes
+        //------------------
+        //Spawn Gold
+        spawnHex(islandHexSpawns, goldHex, 1, false);
+        //Spawn Forest
+        spawnHex(islandHexSpawns, forestHex, 1, false);
+        //Spawn Fields
+        spawnHex(islandHexSpawns, fieldHex, 1, false);
+        //Spawn Hills
+        spawnHex(islandHexSpawns, hillHex, 1, false);
+        //Spawn Dessert
+        spawnHex(islandHexSpawns, dessertHex, 1, false);
+        //Spawn Mountain 
+        spawnHex(islandHexSpawns, mountainHex, 1, false);
+        //Spawn Pasture 
+        spawnHex(islandHexSpawns, pastureHex, 2, false);
+    }
+
+    void spawnHex(List<Transform> spawnPositions, GameObject hexToSpawn, int numToSpawn, bool isOnMainBoard)
+    {
+       for( int i = 0; i < numToSpawn; i++)
         {
-            foreach(GameObject spawnedObject in spawned)
+            Transform targetTransform = spawnPositions[Random.Range(0, spawnPositions.Count)];
+            GameObject spawnedHex = Instantiate(hexToSpawn, targetTransform.position, Quaternion.identity, targetTransform);
+
+            if (isOnMainBoard)
             {
-                Destroy(spawnedObject);
+                spawnedMainBoardHexes.Add(spawnedHex);
             }
-            
-            //foreach(GameObject numberTile in spawnedNumbers)
-            //{
-            //    Destroy(numberTile);
-            //}
-        }
-        
-        foreach (Transform spawnLocation in hexSpawnLocations)
-        {
-            if(spawnLocation != null)
+            else
             {
-                GameObject spawnedHex = Instantiate(hexPieces[Random.Range(0, hexPieces.Count)], spawnLocation.position, Quaternion.identity, spawnLocation);
-                spawned.Add(spawnedHex);
+                spawnedIslandHexes.Add(spawnedHex);
             }
-           
+
+            spawnPositions.Remove(targetTransform);
         }
-        
     }
 }
