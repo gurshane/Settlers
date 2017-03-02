9f83560cae218a8384eda2dc782a73b4bfe689fd
Dedie Kanda <dolente@live.com>
2017-03-02 13:59:21 -0500

    HUD updates

--- Assets/Scripts/UI/UIElement.cs
+++ Assets/Scripts/UI/UIElement.cs
@@ -7,25 +7,62 @@ using UnityEngine.EventSystems;
 /// <summary>
 /// A subelement within the UI Canvas.
 /// </summary>
-public class UIElement : MonoBehaviour, IDescribable<string>, IPointerEnterHandler, IPointerExitHandler {
+public abstract class UIElement : MonoBehaviour, IDescribable<string>, IPointerEnterHandler, IPointerExitHandler {
+
+	#region Protected Attributes
+	#endregion
 
 
-	public void showDescription(string p_Parameter)
+	#region Protected Methods
+	#endregion
+
+
+	#region Public Methods
+
+	/// <summary>
+	/// Updates the UI Element
+	/// </summary>
+	/// <param name="p_Player">P player.</param>
+	public abstract void uiUpdate (Player p_Player);
+
+	/// <summary>
+	/// Shows the description of the UI element being hovered
+	/// </summary>
+	/// <param name="p_Parameter">P parameter.</param>
+	public virtual void showDescription(string p_Parameter)
 	{
 		Descriptor.textComponent.text = p_Parameter;
 		Descriptor.transformComponent.position = Input.mousePosition + getPositionOffset ();
 	}
+		
 
-	public void OnPointerEnter(PointerEventData p_EventData)
+	/// <summary>
+	/// Raises the pointer enter event. Makes descriptor print the gameObject name
+	/// </summary>
+	/// <param name="p_EventData">P event data.</param>
+	public virtual void OnPointerEnter(PointerEventData p_EventData)
 	{
 		showDescription(name);
 	}
 
-	public void OnPointerExit(PointerEventData p_EventData)
+	/// <summary>
+	/// Raises the pointer exit event. Makes the descriptor empty
+	/// </summary>
+	/// <param name="p_EventData">P event data.</param>
+	public virtual void OnPointerExit(PointerEventData p_EventData)
 	{
 		showDescription ("");
 	}
-		
+
+	#endregion
+
+
+	#region Private Methods
+
+	/// <summary>
+	/// Offsets the descriptor text based on mouse position
+	/// </summary>
+	/// <returns>The position offset.</returns>
 	private Vector3 getPositionOffset()
 	{
 		Vector3 returnVec = new Vector3 (0, 0, 0);
@@ -42,4 +79,6 @@ public class UIElement : MonoBehaviour, IDescribable<string>, IPointerEnterHandl
 
 		return returnVec;
 	}
+
+	#endregion
 }
