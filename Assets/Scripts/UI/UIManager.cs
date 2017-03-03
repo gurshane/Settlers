23f7ccf7274716ed47706bc80aceb4a1f66a07cb
Dedie Kanda <dolente@live.com>
2017-03-03 11:37:41 -0500

    Networked UI up

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -20,6 +20,7 @@ public class UIManager : MonoBehaviour {
 	/// <summary>
 	/// The player which this UI displays the info of
 	/// </summary>
+	[SerializeField]
 	private Player _CurrentPlayer;
 
 	#endregion
@@ -49,13 +50,30 @@ public class UIManager : MonoBehaviour {
 	/// Panel containing info of which player's turn it is and the name of the current player
 	/// </summary>
 	[SerializeField]
-	private Transform _CurrentPlayerPanel;
+	private UIElement _MyPlayerPanel;
 
 	/// <summary>
 	/// Panel containing Barbarian slider
 	/// </summary>
 	[SerializeField]
 	private Transform _BarbarianPanel;
+
+	/// <summary>
+	/// The context panel which displays when a board vertex or edge is clicked
+	/// </summary>
+	[SerializeField]
+	private Transform _ContextPanel;
+
+	/// <summary>
+	/// Contents within the ContextPanel. Parent of the VertexButtons and the EdgeButtons panels
+	/// </summary>
+	[SerializeField]
+	private Transform _ContextContentPanel;
+
+	[SerializeField]
+	private Transform _VertexButtonsPanel;
+	[SerializeField]
+	private Transform _EdgeButtonsPanel;
 
 	#endregion
 
@@ -64,8 +82,15 @@ public class UIManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-		//_GameManager = GameObject.FindGameObjectWithTag("GameManager");
+		// Hide ContextPanel and its contents on startup
+		setContextPanelChildren();
 
+		_ContextPanel.gameObject.SetActive (false);
+		_VertexButtonsPanel.gameObject.SetActive (false);
+		_EdgeButtonsPanel.gameObject.SetActive (false);
+
+
+		//_GameManager = GameObject.FindGameObjectWithTag("GameManager");
 		//setPlayerInfoPanels();
 
 	}
@@ -73,11 +98,11 @@ public class UIManager : MonoBehaviour {
 
 
 
-	#region Private Methods
+	#region Update Methods
 	/// <summary>
 	/// Updates the resources of the currentPlayer to be displayed on the UI
 	/// </summary>
-	private void updateResources()
+	public void updateResources()
 	{
 		foreach (Transform resource in _ResourcesPanel)
 		{
@@ -88,7 +113,7 @@ public class UIManager : MonoBehaviour {
 	/// <summary>
 	/// Updates the commodities of the currentPlayer to be displayed on the UI
 	/// </summary>
-	private void updateCommodities()
+	public void updateCommodities()
 	{
 		foreach (Transform commodity in _CommoditiesPanel)
 		{
@@ -97,10 +122,19 @@ public class UIManager : MonoBehaviour {
 	}
 
 	/// <summary>
+	/// Updates the current player panel to be displayed on the UI
+	/// </summary>
+	public void updateMyPlayerPanel()
+	{
+		// Update the CurrentPlayer panel using information from this instance's CurrentPlayer attribute
+		_MyPlayerPanel.uiUpdate (_CurrentPlayer);
+	}
+
+	/// <summary>
 	/// Updates the player info panels to be displayed on the UI
 	/// </summary>
 	/*
-	private void updatePlayerInfoPanels ()
+	public void updatePlayerInfoPanels ()
 	{
 		// Keep track of index to assign a player to a PlayerInfoPanel
 		int pIndex = 0;
@@ -129,7 +163,7 @@ public class UIManager : MonoBehaviour {
 	/// <summary>
 	/// Assigns each available Player to each available PlayerInfoPanel
 	/// </summary>
-	private void setPlayerInfoPanels()
+	public void setPlayerInfoPanels()
 	{
 		// Keep track of index to assign a player to a PlayerInfoPanel
 		int pIndex = 0;
@@ -155,25 +189,99 @@ public class UIManager : MonoBehaviour {
 		}
 	}*/
 
-
-	private void highlightGameObject()
+	/// <summary>
+	/// Handles response for when a Vertex or Edge on the board is clicked
+	/// </summary>
+	public void boardClicked(string p_TargetTag)
 	{
-		if (Input.GetButtonDown("Fire1"))
+		// Set Context Panel to Active
+		_ContextPanel.gameObject.SetActive (true);
+
+		// Displace Panel based on Mouse position when the click occurred
+		_ContextPanel.position = Input.mousePosition;
+		_ContextPanel.position += offsetContextPanel ();
+
+		// Depending on Target's tag, display either the list of options for a Vertex or an Edge
+		switch (p_TargetTag) 
 		{
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			RaycastHit impact;
-			if (Physics.Raycast(ray, out impact))
-			{
-				//_GameManager.GetComponent<GlobalNetworkManager>().CmdHighlightThis( impact.collider.gameObject);
-			}
+		case "Vertex":
+			_VertexButtonsPanel.gameObject.SetActive (true);
+			_EdgeButtonsPanel.gameObject.SetActive (false);
+			break;
+		case "Edge":
+			_VertexButtonsPanel.gameObject.SetActive (false);
+			_EdgeButtonsPanel.gameObject.SetActive (true);
+			break;
+		default:
+			break;
 		}
 	}
+
 	#endregion
+
+	// DEBUGGING
+	/// <summary>
+	/// Checks if the _CurrentPlayer has been instantiated or not
+	/// </summary>
+	/// <returns><c>true</c>, if player check was nulled, <c>false</c> otherwise.</returns>
+	private bool isCurrentPlayerNull()
+	{
+		return (_CurrentPlayer == null);
+	}
 		
+	private Vector3 offsetContextPanel()
+	{
+		Vector3 rVec = new Vector3 (0, 0, 0);
+
+		if (Input.mousePosition.y > 4 * (Screen.height/5) ) 
+		{
+			rVec.y = -80f;
+		}
+
+		else
+		{
+			rVec.y = 80f;
+		}
+
+		return rVec;
+	}
+
+
+	#region Setters
+
+	/// <summary>
+	/// Sets the current player attribute of this UIManager instance to the parameter
+	/// </summary>
+	/// <param name="p_CurrentPlayer">P current player.</param>
+	public void setCurrentPlayer(Player p_CurrentPlayer)
+	{
+		_CurrentPlayer = p_CurrentPlayer;
+	}
+
+	/// <summary>
+	/// Sets the context panel contents for selecting options of either a vertex or edge
+	/// </summary>
+	private void setContextPanelChildren()
+	{
+		_VertexButtonsPanel = _ContextContentPanel.GetChild (0);
+		_EdgeButtonsPanel = _ContextContentPanel.GetChild (1);
+	}
+
+	#endregion
 
 	// Update is called once per frame
 	void Update () {
 
-		highlightGameObject ();
+		// DEBUGGING -----
+
+
+		// Check whether _CurrentPlayer has been initialised
+		if (!isCurrentPlayerNull ()) 
+		{
+			updateResources ();
+			updateCommodities ();
+			updateMyPlayerPanel ();
+		}
+	
 	}
 }
