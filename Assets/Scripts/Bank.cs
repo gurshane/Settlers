32b932bc727dc3a5bfbaaec45730b6d176ce4069
AlexChung1995 <alex.chung@mail.mcgill.ca>
2017-03-12 13:53:15 -0500

    bullshit

--- Assets/Scripts/Bank.cs
+++ Assets/Scripts/Bank.cs
@@ -27,6 +27,7 @@ public class Bank : NetworkBehaviour {
             commodities[i] = 12;
         }
 
+
         scienceCards = new List<Enums.ProgressCardName>();
         scienceCards.Add(Enums.ProgressCardName.ALCHEMIST);
         scienceCards.Add(Enums.ProgressCardName.ALCHEMIST);
