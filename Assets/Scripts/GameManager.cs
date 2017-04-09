521ab9c387093a008d54ab034ae2bd1d114bd18d
Thomas Jansen <thomas@mistplay.com>
2017-04-09 14:51:02 -0400

    Fixing authority stuff

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -145,9 +145,7 @@ public class GameManager : NetworkBehaviour {
 
     public void setSpecialTurn(int turn, bool server) {
         // Assign client authority
-        assignAuthority(server);
         playerTurn = turn;
-        removeAuthority(server);
     }
 	
     // Move to next turn based on game phase
@@ -246,9 +244,7 @@ public class GameManager : NetworkBehaviour {
 	}
 
     public void setGamePhase(GamePhase gPhase, bool server) {
-        assignAuthority(server);
         this.gamePhase = gPhase;
-        removeAuthority(server);
     }
 
 	public Hex getRobberLocation() {
