819c953bcb4226d877d6a98c2b340dab05bdc1bb
Dedie Kanda <dolente@live.com>
2017-04-12 12:38:52 -0400

    Player trade

---
+++ Assets/Scripts/UI/UITradeNotification.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITradeNotification : MonoBehaviour {
+
+	public Transform _GoldImage;
+
+	public Transform _OreImage;
+	public Transform _LumberImage;
+	public Transform _WoolImage;
+	public Transform _BrickImage;
+	public Transform _GrainImage;
+
+	public Transform _ClothImage;
+	public Transform _CoinImage;
+	public Transform _PaperImage;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void updateGoldText(int gold)
+	{
+		_GoldImage.GetChild (0).GetComponent<Text> ().text = "" + gold;
+	}
+
+	public void updateResourcesText(int[] resources)
+	{
+		_OreImage.GetChild (0).GetComponent<Text> ().text = "" + resources [0];
+		_LumberImage.GetChild (0).GetComponent<Text> ().text = "" + resources [1];
+		_WoolImage.GetChild (0).GetComponent<Text> ().text = "" + resources [2];
+		_BrickImage.GetChild (0).GetComponent<Text> ().text = "" + resources [3];
+		_GrainImage.GetChild (0).GetComponent<Text> ().text = "" + resources [4];
+	}
+
+	public void updateCommoditiesText(int[] commodities)
+	{
+		_ClothImage.GetChild (0).GetComponent<Text> ().text = "" + commodities [0];
+		_CoinImage.GetChild (0).GetComponent<Text> ().text = "" + commodities [1];
+		_PaperImage.GetChild (0).GetComponent<Text> ().text = "" + commodities [2];
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
--- Assets/Scripts/UI/UITradeOfferingPanel.cs
+++ Assets/Scripts/UI/UITradeOfferingPanel.cs
@@ -73,27 +73,27 @@ public class UITradeOfferingPanel : MonoBehaviour {
 		{
 		case 0:
 			numText = _OreSlider.transform.GetChild (3).GetComponent<Text> ();
-			numText.text = "" + _OreSlider.value;
+			numText.text = "-" + _OreSlider.value;
 			break;
 		case 1:
 			numText = _LumberSlider.transform.GetChild (3).GetComponent<Text> ();
-			numText.text = "" + _LumberSlider.value;
+			numText.text = "-" + _LumberSlider.value;
 			break;
 		case 2:
 			numText = _WoolSlider.transform.GetChild (3).GetComponent<Text> ();
-			numText.text = "" + _WoolSlider.value;
+			numText.text = "-" + _WoolSlider.value;
 			break;
 		case 3:
 			numText = _BrickSlider.transform.GetChild (3).GetComponent<Text> ();
-			numText.text = "" + _BrickSlider.value;
+			numText.text = "-" + _BrickSlider.value;
 			break;
 		case 4:
 			numText = _GrainSlider.transform.GetChild (3).GetComponent<Text> ();
-			numText.text = "" + _GrainSlider.value;
+			numText.text = "-" + _GrainSlider.value;
 			break;
 		case 5:
 			numText = _GoldSlider.transform.GetChild (3).GetComponent<Text> ();
-			numText.text = "" + _GoldSlider.value;
+			numText.text = "-" + _GoldSlider.value;
 			break;
 		default:
 			break;
